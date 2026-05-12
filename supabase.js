0b390259912392b9ab3ced9747ade458f03da18f
grendil13-stack <grendil13@gmail.com>
2026-05-11 23:31:49 -0400

    fix supabase client using correct env variables

--- supabase.js
+++ supabase.js
@@ -1,8 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-export const supabase = createClient(
-  __SUPABASE_URL__,
-  __SUPABASE_ANON_KEY__,
-);
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
