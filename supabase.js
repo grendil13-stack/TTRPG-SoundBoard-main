dee26b4001ae1bc008f0eae4e26eeb6ccd581a17
grendil13-stack <grendil13@gmail.com>
2026-05-11 23:26:29 -0400

    fix environment variable names for Vite

--- supabase.js
+++ supabase.js
@@ -4,3 +4,5 @@ export const supabase = createClient(
   __SUPABASE_URL__,
   __SUPABASE_ANON_KEY__,
 );
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
