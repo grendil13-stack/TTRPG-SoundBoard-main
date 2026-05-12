ec5ed3d5ccab395c8f4f7e226f929ba27aa05707
grendil13-stack <grendil13@gmail.com>
2026-05-12 00:04:04 -0400

    hardcode both keys to debug vercel cache

--- supabase.js
+++ supabase.js
@@ -1,8 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = "https://kquiougzmjxtaneeedip.supabase.co";
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
-
-console.log('Supabase URL:', supabaseUrl);
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtxdWlvdWd6bWp4dGFuZWVlZGlwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg0MTg2MzEsImV4cCI6MjA5Mzk5NDYzMX0.HKp-KHbI4QCuyFL0lEjaMf2FHM3fQoc7CyzW9BTQbzM";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
