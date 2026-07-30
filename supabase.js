8c1df35caf79eb1d1ddef828e7474dbd6695f7cf
grendil13-stack <grendil13@gmail.com>
2026-07-29 22:27:59 -0400

    changing how login is handled between app and landing

--- supabase.js
+++ supabase.js
@@ -1,6 +1,14 @@
-import { createClient } from "@supabase/supabase-js";
+import { createBrowserClient } from "@supabase/ssr";
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase = createBrowserClient(supabaseUrl, supabaseAnonKey, {
+  cookieOptions: {
+    domain: ".skaldsoundboard.com",
+    path: "/",
+    sameSite: "lax",
+    secure: true,
+    maxAge: 60 * 60 * 24 * 365,
+  },
+});
